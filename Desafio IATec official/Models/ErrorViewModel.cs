0ab3d8b63d8a425384357556ab07a0a3f3d1ae57
fernandokiosa <65138448+fernandokiosa@users.noreply.github.com>
2022-01-20 15:28:01 -0300

    Adicionar arquivos de projeto.

---
+++ Desafio IATec official/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Desafio_IATec_official.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
